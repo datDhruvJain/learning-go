40e7c5f5dee139a64ed54105dd08914c87bdd58a
dhruv <dhruv>
2021-01-25 20:15:54 +0400

    Do functions

--- FucntionsPractice.go
+++ FucntionsPractice.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+func avg(lol ...float64)(avg_ans float64){
+	var sum float64
+	for i := 0; i< len(lol); i++ {
+		sum += lol[i]
+	}
+	avg_ans = sum/float64(len(lol))
+	return
+}
 
 func main(){
-	
+	println(avg(2,3))
 }
